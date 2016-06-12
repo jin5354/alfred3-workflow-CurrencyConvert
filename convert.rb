f3cc9cc0e2bdb472b516f2708f236833e52279c8
jin5354 <xiaoyanjinx@gmail.com>
2016-06-12 20:37:13 +0800

    add flags & fix bugs
    now it can run

--- convert.rb
+++ convert.rb
@@ -15,40 +15,38 @@ if !ARGV.empty?
 end
 
 if hasARGV
-    workflow.result
-        .uid('bob-belcher')
-        .title(ARGV[0])
-        .subtitle('Head Burger Chef')
-        .quicklookurl('http://www.bobsburgers.com')
-        .type('default')
-        .arg('bob')
-        .valid(true)
-        .icon('bob.png')
-        .mod('cmd', 'Search for Bob', 'search')
-        .text('copy', 'Bob is the best!')
-        .autocomplete('Bob Belcher')
-
-    workflow.result
-        .uid('linda-belcher')
-        .title('Linda')
-        .subtitle('Wife')
-        .quicklookurl('http://www.bobsburgers.com')
-        .type('defaulst')
-        .arg('linda')
-        .valid(true)
-        .icon('linda.png')
-        .mod('cmd', 'Search for Linda', 'search')
-        .text('largetype', 'Linda is the best!')
-        .autocomplete('Linda Belcher')
+    str = ARGV[0].lstrip
+    num = str.match(/^\d+/)
+    cy = str.match(/[a-zA-Z]{3}/)
+    if str.empty? || num.nil? || cy.nil?
+        workflow.result
+            .title('No result')
+            .type('default')
+            .valid(true)
+            .icon('icon.png')
+    else
+        num = num[0]
+        cy = cy[0].upcase
+        uri = URI("http://api.fixer.io/latest?base=#{cy}&symbols=CNY,USD,JPY,EUR,HKD,GBP,CAD")
+        result = JSON.parse(Net::HTTP.get(uri))
+        result['rates'].each do |key, value|
+            workflow.result
+                .title("#{(num.to_i*value).round(2)} #{key}")
+                .subtitle("#{cy} : #{key} = 1 : #{value.round(2)} (Last Update: #{result["date"]})")
+                .valid(true)
+                .icon("flags/#{key}.png")
+        end
+    end
 else
-    uri = URI('http://api.fixer.io/latest?base=CNY')
+    base = 'CNY'
+    uri = URI('http://api.fixer.io/latest?base=CNY&symbols=CNY,USD,JPY,EUR,HKD,GBP,CAD')
     result = JSON.parse(Net::HTTP.get(uri))
     result['rates'].each do |key, value|
         workflow.result
-            .title(key)
-            .subtitle(value)
+            .title("CNY : #{key} = 1 : #{value.round(2)} ")
+            .subtitle("Last Update: #{result["date"]}")
             .valid(true)
-            .icon('bob.png')
+            .icon("flags/#{key}.png")
     end
 end
 
