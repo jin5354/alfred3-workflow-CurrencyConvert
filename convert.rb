6fd8170b287426eea836d6a840e4c9b370a4bc78
jin5354 <xiaoyanjinx@gmail.com>
2018-04-14 22:07:09 +0800

    [feat] #2

--- convert.rb
+++ convert.rb
@@ -15,8 +15,27 @@ units = data['units']
 
 if hasARGV
     str = ARGV[0].lstrip.gsub('$', 'usd').gsub('￥', 'cny').gsub('¥', 'jpy').gsub('£', 'gbp').gsub('€', 'eur')
-    num = str.match(/^\d+/)
-    cy = str.match(/\s*[a-zA-Z]{3}/)
+    to = str.match(/\sto\s/)
+    cy = nil
+    num = nil
+    target = nil
+    if to.nil?
+        num = str.match(/^\d+/)
+        if !num.nil?
+            num = num[0]
+        end
+        cy = str.match(/\s*[a-zA-Z]{3}/)
+        if !cy.nil?
+            cy = cy[0].lstrip.upcase
+        end
+    else
+        matcher = str.match(/^(\d+)\s*([a-zA-Z]{3})\sto\s([a-zA-Z]{3})/)
+        if !matcher.nil?
+            num = matcher[1]
+            cy = matcher[2].lstrip.upcase
+            target = matcher[3].lstrip.upcase
+        end
+    end
     if str.empty? || num.nil? || cy.nil?
         temp = Hash[
             "title" => 'No result',
@@ -26,20 +45,34 @@ if hasARGV
         ]
         output["items"].push(temp)
     else
-        num = num[0]
-        cy = cy[0].lstrip.upcase
-        uri = URI("http://api.fixer.io/latest?base=#{cy}&symbols=#{units.join(',')}")
-        result = JSON.parse(Net::HTTP.get(uri))
-        result['rates'].each do |key, value|
-            temp = Hash[
-                "title" => "#{(num.to_i*value).round(2)} #{key}",
-                "subtitle" => "#{cy} : #{key} = 1 : #{value.round(4)} (Last Update: #{result["date"]})",
-                "icon" => Hash[
-                    "path" => "flags/#{key}.png"
-                ],
-                "arg" => "#{(num.to_i*value).round(2)}"
-            ]
-            output["items"].push(temp)
+        if target.nil?
+            uri = URI("http://api.fixer.io/latest?base=#{cy}&symbols=#{units.join(',')}")
+            result = JSON.parse(Net::HTTP.get(uri))
+            result['rates'].each do |key, value|
+                temp = Hash[
+                    "title" => "#{(num.to_i*value).round(2)} #{key}",
+                    "subtitle" => "#{cy} : #{key} = 1 : #{value.round(4)} (Last Update: #{result["date"]})",
+                    "icon" => Hash[
+                        "path" => "flags/#{key}.png"
+                    ],
+                    "arg" => "#{(num.to_i*value).round(2)}"
+                ]
+                output["items"].push(temp)
+            end
+        else
+            uri = URI("http://api.fixer.io/latest?base=#{cy}&symbols=#{target}")
+            result = JSON.parse(Net::HTTP.get(uri))
+            result['rates'].each do |key, value|
+                temp = Hash[
+                    "title" => "#{(num.to_i*value).round(2)} #{key}",
+                    "subtitle" => "#{cy} : #{key} = 1 : #{value.round(4)} (Last Update: #{result["date"]})",
+                    "icon" => Hash[
+                        "path" => "flags/#{key}.png"
+                    ],
+                    "arg" => "#{(num.to_i*value).round(2)}"
+                ]
+                output["items"].push(temp)
+            end
         end
     end
 else
