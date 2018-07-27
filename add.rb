8b556c9227fd6041b18a9208de5f296fe9dbc99f
Kris McCann <kris@8pi.ca>
2018-07-27 03:56:34 -0300

    added bitcoin support through coindesk

--- add.rb
+++ add.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'date'
 
 output = Hash["items" => []]
 data = JSON.parse(File.read('data.json'))
@@ -14,6 +15,7 @@ result['rates'].each do |key, value|
         temp = Hash[
             "title" => "#{key}",
             "subtitle" => "#{base} : #{key} = 1 : #{value.round(4)} Last Update: #{result["date"]}",
+            "rate" => value,
             "icon" => Hash[
                 "path" => "flags/#{key}.png"
             ],
@@ -23,4 +25,19 @@ result['rates'].each do |key, value|
     end
 end
 
+uri = URI("https://api.coindesk.com/v1/bpi/currentprice/#{base}.json")
+result = JSON.parse(Net::HTTP.get(uri))
+updated = DateTime.parse(result["time"]["updated"]).strftime("%Y-%m-%d")
+rate = result['bpi'][base]['rate_float'].round(4)
+output["items"].push(Hash[
+    "title" => "BTC",
+    "subtitle" => "#{base} : BTC = 1 : #{rate} Last Update: #{updated}",
+    "rate" => rate,
+    "icon" => Hash[
+        "path" => "flags/BTC.png"
+    ],
+    "arg" => "BTC"
+])
+
+output["items"] = output["items"].sort{ |a, b| a['arg'] <=> b['arg'] }
 print output.to_json
--- base.rb
+++ base.rb
@@ -7,7 +7,11 @@ data = JSON.parse(File.read('data.json'))
 base = data['base']
 units = data['units']
 
-uri = URI("https://exchangeratesapi.io/api/latest?base=#{base}")
+if base.upcase.eql?("BTC")
+    uri = URI("https://exchangeratesapi.io/api/latest?base=USD")
+else
+    uri = URI("https://exchangeratesapi.io/api/latest?base=#{base}")
+end
 result = JSON.parse(Net::HTTP.get(uri))
 result['rates'].each do |key, value|
     temp = Hash[
@@ -19,5 +23,15 @@ result['rates'].each do |key, value|
     ]
     output["items"].push(temp)
 end
+    
+output["items"].push(Hash[
+    "title" => "BTC",
+    "icon" => Hash[
+        "path" => "flags/BTC.png"
+    ],
+    "arg" => "BTC"
+])
+
+output["items"] = output["items"].sort{ |a, b| a['arg'] <=> b['arg'] }
 
 print output.to_json
